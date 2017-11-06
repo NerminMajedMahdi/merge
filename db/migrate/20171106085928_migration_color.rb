caa0d00f77af8c5460161d10ac4a4770bb4f2352
NerminMajedMahdi <mahdi.nirmeen@gmail.com>
2017-11-06 19:28:30 +0200

    resubmission 4.2

--- db/migrate/20171106085928_migration_color.rb
+++
@@ -1,4 +0,0 @@
-class MigrationColor < ActiveRecord::Migration[5.1]
-  def change
-  end
-end
--- db/migrate/20171106170916_rename_colors_in_products.rb
+++ db/migrate/20171106170916_rename_colors_in_products.rb
@@ -1,4 +1,6 @@
 class RenameColorsInProducts < ActiveRecord::Migration[5.1]
   def change
+    rename_column :products, :color, :colour
+  
   end
 end
