7f150efbb7d693059bf0f42fa49f7c0f1f8c336c
NerminMajedMahdi <mahdi.nirmeen@gmail.com>
2017-11-14 09:05:50 +0200

    task 5.4 after modifications

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -6,7 +6,7 @@ class Ability
     if user.admin?
       can :manage, :all 
     else
-      can :manage, User, id: user.id
+       can [:edit, :read, :destroy], User, :id => user.id
     end
   end
 end
