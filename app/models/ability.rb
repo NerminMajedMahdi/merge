116d92bf5fcdfc4778213778a3f19ce0c70fbf7e
NerminMajedMahdi <mahdi.nirmeen@gmail.com>
2017-11-14 18:10:45 +0200

    5.4 modification test

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -2,15 +2,15 @@ class Ability
   include CanCanCan::Ability
 
 
+  
+  include CanCan::Ability
+
   def initialize(user)
-    can :read, :all . # permissions for every user, even if not logged in    
-    if user.present?  # additional permissions for logged in users (they can manage their posts)
-      can :manage, Post, user_id: user.id 
-      if user.admin?  # additional permissions for administrators
-        can :manage, :all
-      end
-    end
+    user ||= User.new # guest user (not logged in)
+    can :manage, User, id: user.id
   end
+
+end
 
 
 
