8c8d559716805db51f5a1333e25aab22fed8490a
NerminMajedMahdi <mahdi.nirmeen@gmail.com>
2017-11-19 10:53:49 +0200

    5.4 and 5.5 modifications

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -4,7 +4,8 @@ class Ability
 
  def initialize(user)
      user ||= User.new # guest user (not logged in)
-     can :manage, User, id: user.id
+
+    # can :manage, User, id: user.id
     #if user.admin?
      # can :manage, :all 
    # else
