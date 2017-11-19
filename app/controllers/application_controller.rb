58d20065a8de53fb35744e8273f6c03e65e9f2ae
NerminMajedMahdi <mahdi.nirmeen@gmail.com>
2017-11-19 11:41:15 +0200

    4.6 modifcation

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
-  rescue_from CanCan::AccessDenied do |exception|
-    redirect_to main_app.root_url, alert: exception.message
-  end
 
   # adding field first_name  last_name to devise
  # before_action :configure_permitted_parameters, if: :devise_controller?
